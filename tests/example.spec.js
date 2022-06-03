6a9c7856ca634cd5c6c80a81607808d109a1710a
maniziva <manizivamsd@gmail.com>
2022-06-03 22:05:12 +0530

    initial commit

---
+++ tests/example.spec.js
@@ -0,0 +1,18 @@
+const { test, expect } = require('@playwright/test');
+
+test.only('Example Test - 1', async ({ page }) => {
+  await page.goto('https://demoqa.com/automation-practice-form');
+  const firstName = page.locator('#firstName');
+  const lastName = page.locator('#lastName');
+  const userEmail = page.locator('#userEmail');
+  //await firstName.click();
+  await firstName.type("Manikandan");
+  await lastName.type("Adaikalam");
+  await userEmail.type("maniziva@gmail.com");
+  await page.selectOption('#gender-radio-1', { label: 'Male' });
+
+});
+
+test('Example Test - 2', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+});
